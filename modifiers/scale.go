77f9da649175b4b6de2c478d5f5fd919bc3c9ee9
Poletansky Viktor <poletansky@gmail.com>
2023-01-22 17:52:49 +0300

    dev: implement i,d scalex

--- modifiers/scale.go
+++ modifiers/scale.go
@@ -15,27 +15,28 @@ type ScaleX struct {
 }
 
 func (m ScaleX) Modify() (*webp.AnimationEncoder, error) {
-	enc, err := webp.NewAnimationEncoder(m.In.CanvasWidth, m.In.CanvasHeight, 0, 0)
+	newWidth := int(float32(m.In.CanvasWidth) * m.Scale)
+	stride := newWidth * 4
+
+	enc, err := webp.NewAnimationEncoder(newWidth, m.In.CanvasHeight, 0, 0)
 	if err != nil {
 		return nil, err
 	}
 
 	for i, frame := range m.In.Image {
-		buf := append([]uint8{}, frame.Pix...)
-		for row := 0; row < m.In.CanvasHeight; row++ {
-			stride := row * frame.Stride
-			for i, j := 0, frame.Stride-4; i < j; i, j = i+4, j-4 {
-				buf[stride+i+0], buf[stride+j+0] = buf[stride+j+0], buf[stride+i+0]
-				buf[stride+i+1], buf[stride+j+1] = buf[stride+j+1], buf[stride+i+1]
-				buf[stride+i+2], buf[stride+j+2] = buf[stride+j+2], buf[stride+i+2]
-				buf[stride+i+3], buf[stride+j+3] = buf[stride+j+3], buf[stride+i+3]
+		buf := make([]uint8, stride*m.In.CanvasHeight)
+		for j := 0; j < m.In.CanvasHeight; j++ {
+			for i := 0; i < newWidth; i++ {
+				for k := 0; k < 4; k++ {
+					buf[j*stride+i*4+k] = frame.Pix[j*frame.Stride+int(float32(i)/m.Scale)*4+k]
+				}
 			}
 		}
 
 		res := &image.RGBA{
 			Pix:    buf,
-			Stride: frame.Stride,
-			Rect:   frame.Rect,
+			Stride: stride,
+			Rect:   image.Rect(0, 0, newWidth, m.In.CanvasHeight),
 		}
 
 		if err := enc.AddFrame(res, time.Duration(m.In.Timestamp[i])*time.Millisecond); err != nil {
