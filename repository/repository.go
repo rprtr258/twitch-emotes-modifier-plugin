683e978c0b544d10d9fd1a0c188486cac9d1fb16
Poletansky Viktor <poletansky@gmail.com>
2023-02-11 16:26:38 +0300

    dev: add get emote id by name

--- repository/repository.go
+++ repository/repository.go
@@ -1,12 +1,10 @@
 package repository
 
 import (
-	"fmt"
-	"io"
-	"net/http"
 	"os"
 
 	"github.com/rprtr258/twitch-emotes-modifier-plugin/pkg/webp"
+	"github.com/rprtr258/twitch-emotes-modifier-plugin/repository/seventv"
 )
 
 func loadObject(objectID string) (*webp.Animation, error) {
@@ -58,34 +56,10 @@ func IsCached(emoteID string) bool {
 	return err == nil
 }
 
-func download7tvEmote(emoteID, outObjectID string) ([]byte, error) {
-	emoteUrl := fmt.Sprintf("https://cdn.7tv.app/emote/%s/4x", emoteID)
-	resp, err := http.Get(emoteUrl)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	// var extension string
-	switch imageFormat := resp.Header.Get("Content-type"); imageFormat {
-	case "image/webp":
-		// extension = "webp"
-	default:
-		return nil, fmt.Errorf("unknown image format: %s", imageFormat)
-	}
-
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
-}
-
 // TODO: load only if id
 func Emote(emoteID string) (*webp.Animation, error) {
 	if !IsCached(emoteID) {
-		data, err := download7tvEmote(emoteID, emoteID)
+		data, err := seventv.Repository{}.Download7tvEmote(emoteID, emoteID)
 		if err != nil {
 			return nil, err
 		}
---
+++ repository/seventv/seventv.go
@@ -0,0 +1,192 @@
+package seventv
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type User struct {
+	ID          string `json:"id"`
+	Username    string `json:"username"`
+	DisplayName string `json:"display_name"`
+	CreatedAt   int64  `json:"created_at"`
+	AvatarURL   string `json:"avatar_url"`
+	EmoteSets   []struct {
+		ID       string `json:"id"`
+		Name     string `json:"name"`
+		Flags    int    `json:"flags"`
+		Capacity int    `json:"capacity"`
+	} `json:"emote_sets"`
+	Editors []struct {
+		ID          string `json:"id"`
+		Permissions int    `json:"permissions"`
+		Visible     bool   `json:"visible"`
+		AddedAt     int64  `json:"added_at"`
+	} `json:"editors"`
+	Roles       []string `json:"roles"`
+	Connections []struct {
+		ID            string      `json:"id"`
+		Platform      string      `json:"platform"`
+		Username      string      `json:"username"`
+		DisplayName   string      `json:"display_name"`
+		LinkedAt      int64       `json:"linked_at"`
+		EmoteCapacity int         `json:"emote_capacity"`
+		EmoteSetID    interface{} `json:"emote_set_id"`
+		EmoteSet      struct {
+			ID         string        `json:"id"`
+			Name       string        `json:"name"`
+			Flags      int           `json:"flags"`
+			Tags       []interface{} `json:"tags"`
+			Immutable  bool          `json:"immutable"`
+			Privileged bool          `json:"privileged"`
+			Capacity   int           `json:"capacity"`
+			Owner      interface{}   `json:"owner"`
+		} `json:"emote_set"`
+	} `json:"connections"`
+}
+
+type EmoteSet struct {
+	ID         string        `json:"id"`
+	Name       string        `json:"name"`
+	Flags      int           `json:"flags"`
+	Tags       []interface{} `json:"tags"`
+	Immutable  bool          `json:"immutable"`
+	Privileged bool          `json:"privileged"`
+	Emotes     []struct {
+		ID        string      `json:"id"`
+		Name      string      `json:"name"`
+		Flags     int         `json:"flags"`
+		Timestamp int64       `json:"timestamp"`
+		ActorID   interface{} `json:"actor_id"`
+		Data      struct {
+			ID        string   `json:"id"`
+			Name      string   `json:"name"`
+			Flags     int      `json:"flags"`
+			Lifecycle int      `json:"lifecycle"`
+			State     []string `json:"state"`
+			Listed    bool     `json:"listed"`
+			Animated  bool     `json:"animated"`
+			Owner     struct {
+				ID          string `json:"id"`
+				Username    string `json:"username"`
+				DisplayName string `json:"display_name"`
+				AvatarURL   string `json:"avatar_url"`
+				Style       struct {
+				} `json:"style"`
+				Roles []string `json:"roles"`
+			} `json:"owner"`
+			Host struct {
+				URL   string `json:"url"`
+				Files []struct {
+					Name       string `json:"name"`
+					StaticName string `json:"static_name"`
+					Width      int    `json:"width"`
+					Height     int    `json:"height"`
+					FrameCount int    `json:"frame_count"`
+					Size       int    `json:"size"`
+					Format     string `json:"format"`
+				} `json:"files"`
+			} `json:"host"`
+		} `json:"data,omitempty"`
+	} `json:"emotes"`
+	EmoteCount int `json:"emote_count"`
+	Capacity   int `json:"capacity"`
+	Owner      struct {
+		ID          string `json:"id"`
+		Username    string `json:"username"`
+		DisplayName string `json:"display_name"`
+		AvatarURL   string `json:"avatar_url"`
+		Style       struct {
+		} `json:"style"`
+		Roles []string `json:"roles"`
+	} `json:"owner"`
+}
+
+type Repository struct {
+}
+
+func (Repository) Download7tvEmote(emoteID, outObjectID string) ([]byte, error) {
+	resp, err := http.Get(fmt.Sprintf("https://cdn.7tv.app/emote/%s/4x", emoteID))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// var extension string
+	switch imageFormat := resp.Header.Get("Content-type"); imageFormat {
+	case "image/webp":
+		// extension = "webp"
+	default:
+		return nil, fmt.Errorf("unknown image format: %s", imageFormat)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (Repository) GetUser(userID string) (User, error) {
+	userResp, err := http.Get(fmt.Sprintf("https://7tv.io/v3/users/%s", userID))
+	if err != nil {
+		return User{}, err
+	}
+	defer userResp.Body.Close()
+
+	var user User
+	if err := json.NewDecoder(userResp.Body).Decode(&user); err != nil {
+		return User{}, err
+	}
+
+	return user, nil
+}
+
+func (r Repository) GetEmoteSet(userID string) (EmoteSet, error) {
+	userResp, err := http.Get(fmt.Sprintf("https://7tv.io/v3/emote-sets/%s", userID))
+	if err != nil {
+		return EmoteSet{}, err
+	}
+	defer userResp.Body.Close()
+
+	var emoteSet EmoteSet
+	if err := json.NewDecoder(userResp.Body).Decode(&emoteSet); err != nil {
+		return EmoteSet{}, err
+	}
+
+	return emoteSet, nil
+}
+
+func (r Repository) GetEmoteID(userID string, emoteName string) (string, error) {
+	user, err := r.GetUser(userID)
+	if err != nil {
+		return "", err
+	}
+
+	var emoteSetID string
+	for _, connection := range user.Connections {
+		if connection.Platform == "TWITCH" {
+			emoteSetID = connection.EmoteSet.ID
+		}
+	}
+	if emoteSetID == "" {
+		return "", errors.New("no emote set found for twitch")
+	}
+
+	emoteSet, err := r.GetEmoteSet(emoteSetID)
+	if err != nil {
+		return "", err
+	}
+
+	for _, emote := range emoteSet.Emotes {
+		if emote.Name == emoteName {
+			return emote.ID, nil
+		}
+	}
+
+	return "", errors.New("no emote found")
+}
